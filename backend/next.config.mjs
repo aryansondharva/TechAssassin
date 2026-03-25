6e5826a4fad18612c6df6f4a3906c48a7276c486
ARYAN SONDHARVA <aryansondharva25@gmail.com>
2026-03-25 17:29:07 +0530

    update the package.json

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    esmExternals: false,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/*': './*',
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
